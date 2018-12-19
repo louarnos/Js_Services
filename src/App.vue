238c7f4b6566908d524f02f5f9bc9bd59dff6606
louarnos <louarnos@gmail.com>
2018-12-18 22:53:22 -0500

    Get navbar and jumbo setup
    Need to scale for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <v-app>
-    <v-toolbar app color="primary">
-      <v-toolbar-title class="headline nav-text">
-        <span> J's </span>
-        <span class="font-weight-light">Junk Removal Service</span>
-      </v-toolbar-title>
+  <v-app top=fixed-navbar>
+    <v-toolbar app :height="'100px'">
+      <v-toolbar-sideicon width="30%">
+        <img class="nav-logo" :src="require('@/assets/logo.jpg')"/>
+      </v-toolbar-sideicon>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down nav-text">
+        <v-btn class="nav-text" flat>Link One</v-btn>
+        <v-btn class="nav-text" flat>Link Two</v-btn>
+        <v-btn class="nav-text" flat>Link Three</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-    <div style="min-height:1500px" color="primary">
-        <img :src="require('@/assets/logo.jpg')"></img>
+
+    <div class="main">
+      <v-jumbotron
+        :gradient="gradient"
+        dark
+        height="600px"
+        :src="require('@/assets/truck.jpg')" >
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex text-xs-center>
+              <h3 class="jumbo-title">
+                <span> J's </span>
+                <span class="font-weight-light">Junk Removal Service</span>
+              </h3>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
     </div>
+
+    <!--
+      <v-toolbar-title class="nav-title headline">
+      </v-toolbar-title>
+      -->
+
+
+
   </v-app>
 </template>
 
@@ -33,7 +54,19 @@ export default {
 }
 </script>
 <style>
-  .nav-text {
-      color: #64DD17;
+  .main {
+      margin-top: 100px;
+      background-color: black;
+      min-height: 3000px;
+  }
+  .jumbo-title {
+      font-size: 5em;
+  }
+  .nav-logo {
+      max-width: 15%;
+  }
+  img {
+      max-width: 100%;
+      height: auto;
   }
 </style>
